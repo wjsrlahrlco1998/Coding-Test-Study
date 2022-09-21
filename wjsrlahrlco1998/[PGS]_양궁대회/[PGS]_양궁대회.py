8ff85ca1423632caaade2267fbf36bcd3557c691
wjsrlahrlco1998 <wjsrlahrlco@daum.net>
2022-09-21 23:07:44 +0900

    [PGS] 양궁대회 / Level 2 / 6시간

--- wjsrlahrlco1998/[PGS]_양궁대회/[PGS]_양궁대회.py
+++ wjsrlahrlco1998/[PGS]_양궁대회/[PGS]_양궁대회.py
@@ -1,67 +1,58 @@
-def solution(n, info):
-    answer = []
-
-    # 1. 현재 어피치가 획득한 점수
-    apitch_score = 0
-    for i in range(len(info)):
-        if info[i] > 0:
-            apitch_score += 10-i
-
-    # 2. 어피치의 과녁을 참고하여 각 과녁의 점수당 화살의 효율 점수를 구한다.
-    eff_arrow_info = [0] * 11
-    for i in range(len(info)):
-        eff_arrow_score = (10 - i) / (info[i] + 1) # 과녁 점수 / 맞춰야할 화살의 수 = 화살 한 개당의 효율 점수
-        if info[i] > 0: # 만약 화살이 있으면 그 효율은 2배(점수를 뺏기 때문)
-            eff_arrow_score = eff_arrow_score * 2
-        eff_arrow_info[i] = (eff_arrow_score, info[i] + 1, i) # (효율점수, 쏘아야할 화살의 수, 과녁 인덱스)
-
-    # 3. 효율 점수 리스트를 정렬
-    eff_arrow_info = sorted(eff_arrow_info, key = lambda x : (x[0], x[2]), reverse=True)
-    print(eff_arrow_info)
-
-    # 4. 효율 점수 리스트를 바탕으로 과녁 쏘기
+def calcPoint(apeach_board, lion_board):
+    # 1. 어피치와 라이언의 전체 점수를 각각 저장할 변수 선언
+    apeach_score = 0
     lion_score = 0
-    lion_info = [0] * 11
-    limited_n_arrow = n
 
-    for i in eff_arrow_info:
-        if (i[1] > 1) & (limited_n_arrow - i[1] >= 0):
-            lion_info[i[2]] = i[1]
-            lion_score += 10 - i[2]
-            apitch_score -= 10 - i[2]
-            limited_n_arrow -= i[1]
-        elif (limited_n_arrow - i[1] >= 0):
-            lion_info[i[2]] = i[1]
-            lion_score += 10 - i[2]
-            limited_n_arrow -= i[1]
-
-        if limited_n_arrow == 0:
-            break
+    # 2. for 문을 이용하여 과녁의 점수 계산
+    for i in range(11):
+        # 1) 과녁에 꽂힌 화살의 개수 어피치와 라이언 모두 0개로 같은 경우 : 점수변화 없음
+        if apeach_board[i] == lion_board[i] == 0:
+            continue    
+        # 2) 과녁에 꽂힌 화살의 개수가 어피치가 라이언과 같거나 큰 경우 : 어피치의 점수 +
+        if apeach_board[i] >= lion_board[i]: 
+            apeach_score += 10 - i
+        # 3) 과녁에 꽂힌 화살의 개수가 라이언이 더 큰 경우 : 라이언의 점수 +
+        else:
+            lion_score += 10 - i
     
-    if limited_n_arrow > 0:
-        lion_info[-1] += limited_n_arrow
-        limited_n_arrow = 0
+    # 3. 라이언 점수 - 어피치 점수 리턴
+    return lion_score - apeach_score
 
-    if lion_score <= apitch_score:
-        answer = [-1]
-    else:
-        answer = lion_info
+def DFS(idx, arrow_n, apeach_board, lion_board):
+    global answer, point
+
+    # 2. 화살의 개수가 0보다 작은 경우 return
+    if arrow_n < 0:
+        return
+    
+    # 3. 최대깊이(11)까지 탐색이 이루어진 경우 과녁의 점수를 계산
+    if idx > 10:
+        diff = calcPoint(apeach_board, lion_board)
+        # 1) 라이언 - 어피치의 차가 0보다 같거나 작은 경우 과녁을 업데이트 하지않는다([-1] 그대로)
+        if diff <= 0:
+            return
+        # 2) 라이언 - 어피치의 차가 point보다 큰 경우 과녁을 업데이트한다.
+        if diff > point:
+            point = diff
+            answer = [lion_board[i] for i in range(11)]
+            answer[10] += arrow_n
+        return
+    
+    # 4. 재귀 탐색
+    # 1) 라이언이 어피치보다 더 많은 화살을 쏘는 경우 탐색
+    lion_board[10 - idx] = apeach_board[10 - idx] + 1
+    DFS(idx + 1, arrow_n - lion_board[10 - idx], apeach_board, lion_board)
+    # 2) 라이언이 쏘지 않는 경우 탐색
+    lion_board[10 - idx] = 0
+    DFS(idx + 1, arrow_n, apeach_board, lion_board)
+
+def solution(n, info):
+    # 1. DFS에 사용할 전역변수 선언
+    global answer, point
+    answer = [-1]
+    point = 0
+    
+    # 2. DFS(깊이 우선 탐색 시작)
+    DFS(0, n, info, [0 for _ in range(11)])
 
     return answer
-
-print("result")
-print(solution(5, [2,1,1,1,0,0,0,0,0,0,0]))
-print(solution(1, [1,0,0,0,0,0,0,0,0,0,0]))
-print(solution(9, [0,0,1,2,0,1,1,1,1,1,1]))
-print(solution(10, [0,0,0,0,0,0,0,0,3,4,3]))
-print(solution(3, [0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0]))
-print(solution(2, [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]))
-"""
-<메모>
-1. 어피치의 점수 0개를 뺏는 경우 ~ n개를 뺏는 경우
-2. '1'의 과정을 시행해서 라이언의 합계 점수가 어피치의 합계 점수를 넘으면 return
-
-<조건>
-1. 라이언은 가장 큰 점수차로 이기는 경우가 Best
-2. 가장 큰 점수차로 이기는 경우가 여러 개일 경우, 낮은 점수를 많이 쏜 경우가 Best
-"""
