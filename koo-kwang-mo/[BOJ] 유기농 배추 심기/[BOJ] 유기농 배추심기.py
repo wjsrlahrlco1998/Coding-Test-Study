18a14cc0aea156c8ea01aa1dea8c6dafab24d0dc
koo-kwang-mo <dpqpfptmxm3@naver.com>
2022-10-05 02:35:28 +0900

    [BOJ] 유기농 배추심기 수정

--- koo-kwang-mo/[BOJ] 유기농 배추 심기/[BOJ] 유기농 배추심기.py
+++ koo-kwang-mo/[BOJ] 유기농 배추 심기/[BOJ] 유기농 배추심기.py
@@ -1,40 +1,43 @@
-T = int(input()) #테스트케이스의 개수
+# 재귀 limit 설정
+import sys
+sys.setrecursionlimit(10000)
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+### 2
+# dfs 정의
+def dfs(x, y):
+    # 상하좌우 확인을 위해 dx, dy 생성
+    dx = [0,0,-1,1]
+    dy = [1,-1,0,0]
 
-def BFS(x,y):
-    queue = [(x,y)]
-    matrix[x][y] = 0 # 방문처리
+    # 네 방향 탐색
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if (0 <= nx < M) and (0 <= ny < N):  # nx:ny ↔ M:N 범위 참고
+            if graph[ny][nx] == 1:
+                graph[ny][nx] = -1  # 배추가 인접할 때 체커
+                dfs(nx, ny)
 
-    while queue:
-        x,y = queue.pop(0)
+### 1                    
+T = int(input())
 
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if nx < 0 or nx >= M or ny < 0 or ny >= N:
-                continue
-
-            if matrix[nx][ny] == 1 :
-                queue.append((nx,ny))
-                matrix[nx][ny] = 0
-
-# 행렬만들기
 for i in range(T):
-    M, N, K = map(int,input().split())
-    matrix = [[0]*(N) for _ in range(M)]
+    M, N, K = map(int, input().split())  # M:가로, N:세로, K:개수
+    graph = [[0]*M for i in range(N)]
     cnt = 0
 
+    # 배추 위치에 1 표시
     for j in range(K):
-        x,y = map(int, input().split())
-        matrix[x][y] = 1
+        X, Y = map(int, input().split())
+        graph[Y][X] = 1
 
-    for a in range(M):
-        for b in range(N):
-            if matrix[a][b] == 1:
-                BFS(a,b)
+### 3        
+    # dfs 활용해서 배추 그룹 수 세기
+    for x in range(M):
+        for y in range(N):
+            if graph[y][x] == 1:
+                dfs(x, y)
                 cnt += 1
 
+    # 정답을 위한 출력
     print(cnt)
